b0bfeb6dc09fd3dc3573c9a2f0d5ec7331c26fbb
M. Zyon McCalley <zyon.mccalley@gmail.com>
2009-02-27 12:05:01 -0600

    "Added a dash of Ajax"<F12><F11>

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '3bf039de30aad14a07faef8c335fa8ea'
+  protect_from_forgery :secret => '3bf039de30aad14a07faef8c335fa8ea'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,41 @@
 class StoreController < ApplicationController
   def index
   	@products = Product.find_products_for_sale
+  	@cart = find_cart
   	@time = Time.now
   end
   
-
+  def find_cart
+  	unless session[:cart]
+  		session[:cart] = Cart.new
+  	end
+  	session[:cart]
+  end	
+  
+  def add_to_cart
+  	product = Product.find(params[:id])
+  	@cart = find_cart
+  	@cart.add_product(product)
+  	  redirect_to_index
+  rescue ActiveRecord::RecordNotFound
+  	logger.error("Attempt to access invalid product #{params[:id]}")
+  	redirect_to_index("Invalid product")
+  end
+  
+  def empty_cart
+  	session[:cart] = nil
+  	redirect_to_index( "Your cart is currently empty" )
+  end
+  
+private
+  
+  def redirect_to_index(msg = nil)
+  	flash[:notice] = msg if msg
+  	redirect_to :action => 'index'
+  end
+  
+  def find_cart
+  	session[:cart] ||= Cart.new
+  end
+  
 end
