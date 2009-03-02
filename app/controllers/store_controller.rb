c418ea6f9a9e1f0329594c2911ca2d9b3108432c
M. Zyon McCalley <zyon.mccalley@gmail.com>
2009-03-02 12:46:22 -0600

    format for html with ajax

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -17,7 +17,8 @@ class StoreController < ApplicationController
   	@cart = find_cart
   	@current_item = @cart.add_product(product)
   	  respond_to do |format|
-  	  	format.js
+  	  	format.js if request.xhr?
+  	  	format.html {redirect_to_index}
   	  end
   rescue ActiveRecord::RecordNotFound
   	logger.error("Attempt to access invalid product #{params[:id]}")
@@ -26,7 +27,7 @@ class StoreController < ApplicationController
   
   def empty_cart
   	session[:cart] = nil
-  	redirect_to_index( "Your cart is currently empty" )
+  	redirect_to_index
   end
   
 private
