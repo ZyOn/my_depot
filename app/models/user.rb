5f7095e0c75e2b9cdb45e391e2ba6e3276b67f1f
M. Zyon McCalley <zyon.mccalley@gmail.com>
2009-03-26 16:15:06 -0500

    Same user controller

---
+++ app/models/user.rb
@@ -0,0 +1,51 @@
+require 'digest/sha1'
+class User < ActiveRecord::Base
+  
+  validates_presence_of		:name
+  validates_uniqueness_of	:name
+  validates_confirmation_of	:password
+  
+  attr_accessor	:password_confimation
+  
+  validate 	:password_non_block
+  
+  def self.authenticate(name, password)
+  	user = self.find_by_name(name)
+  	if user
+  	  expected_password = encrypted_password(password, user.salt)
+  	  if user.hashed_password != expected_password
+  	  	user = nil
+  	  end
+  	end
+  	user
+  end
+    
+  # 'password' is a virtual attribute
+  def password
+  	@password
+  end
+  
+  def password=(pwd)
+  	@password = pwd
+  	return if pwd.blank?
+  	create_new_salt
+  	self.hashed_password = User.encryptd_password(self.password, self.salt)
+  end
+  
+  
+private
+
+  def password_non_block
+  	errors.add(:password, "Missing password") if hashed_password.blank?
+  end
+  
+  def create_new_salt
+  	self.salt = self.object_id.to_s + rand.to_s
+  end
+  
+  def self.encrypted_password(password, salt)
+  	string_to_hash = password + "wibble" + salt
+  	Digest::SHA1.hexdigest(string_to_hash)
+  end
+    
+end
